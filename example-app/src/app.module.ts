143c6f35e49f023f50314660284639af6dacae36
John Bryan Gaona Trinidad <john.gt81@gmail.com>
2024-06-18 14:01:32 -0500

    feat: add configmodule

--- example-app/src/app.module.ts
+++ example-app/src/app.module.ts
@@ -5,9 +5,17 @@ import { CoursesModule } from './courses/courses.module';
 import { AuthModule } from './auth/auth.module';
 import { VideosModule } from './videos/videos.module';
 import { AwardsModule } from './awards/awards.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CoursesModule, AuthModule, VideosModule, AwardsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    CoursesModule,
+    AuthModule, 
+    VideosModule, 
+    AwardsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- example-app/src/main.ts
+++ example-app/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { json } from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true
+  });
+  app.use(json({limit: '60mb'})); //limit payload request size
+
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI
+  })
+
   app.useGlobalPipes(new ValidationPipe());
   
   const config = new DocumentBuilder()
